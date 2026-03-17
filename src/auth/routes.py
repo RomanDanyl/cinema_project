396c6530decd4b10e3d3aa08594fdb1b3fa2ef92
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2026-03-17 16:59:47 +0200

    create base route for registration

---
+++ src/auth/routes.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models import UserModel
+from src.database import get_db
+from src.auth.schemas import UserRegistrationResponseSchema, UserRegistrationRequestSchema
+
+router = APIRouter(prefix="/accounts")
+
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    summary="User Registration",
+    description="Register a new user with an email and password.",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        409: {
+            "description": "Conflict - User with this email already exists.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "A user with this email test@example.com already exists."
+                    }
+                }
+            },
+        },
+        500: {
+            "description": "Internal Server Error - An error occurred during user creation.",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "An error occurred during user creation."}
+                }
+            },
+        },
+    },
+)
+async def register_user(
+        user_data: UserRegistrationRequestSchema,
+        db: AsyncSession = Depends(get_db)
+) -> UserRegistrationResponseSchema:
+    stmt = select(UserModel).where(UserModel.email == user_data.email)
+    result = await db.execute(stmt)
+    existing_user = result.scalars().first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"A user with this email {user_data.email} already exists.",
+        )
+    try:
+        new_user = UserModel.create(
+            email=user_data.email,
+            raw_password=user_data.password,
+        )
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during user creation.",
+        ) from e
+    else:
+        return UserRegistrationResponseSchema.model_validate(new_user)
