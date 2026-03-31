a41cfa8b28cbb21cddfefe70350dd637edeb76f3
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2026-03-31 15:02:59 +0300

    delete validation from user schema

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -9,16 +9,6 @@ class BaseEmailPasswordSchema(BaseModel):
 
     model_config = {"from_attributes": True}
 
-    @field_validator("email")
-    @classmethod
-    def validate_email(cls, value):
-        return value.lower()
-
-    @field_validator("password")
-    @classmethod
-    def validate_password(cls, value):
-        return validate_password_strength(value)
-
 
 class UserRegistrationRequestSchema(BaseEmailPasswordSchema):
     pass
